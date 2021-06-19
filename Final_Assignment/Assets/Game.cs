fc17670df37097ff59742af61518c99714550fc3
dynle <doyun04180@gmail.com>
2021-06-19 21:55:54 +0900

    change details

--- Final_Assignment/Assets/Game.cs
+++ Final_Assignment/Assets/Game.cs
@@ -9,13 +9,6 @@ using UnityEngine;
 /// </summary>
 public sealed class Game : GameBase
 {
-    // 変数の宣言
-    // int score=0;
-    // string pname = "t93520mw";
-    // string pname = "t17501dl";
-    // string url = "";
-    // string str = "";
-
     const int ENEMY_NUM = 10;
     int[] enemy_x = new int [ENEMY_NUM];
     int[] enemy_y = new int [ENEMY_NUM];
@@ -30,7 +23,6 @@ public sealed class Game : GameBase
 
     int score =0;
     int count =0;
-    // string str="";
 
     int gameState = 0;
 
@@ -40,12 +32,8 @@ public sealed class Game : GameBase
 
     float attack_rate = 0.1f;
 
-    /// <summary>
-    /// 初期化処理
-    /// </summary>
     public override void InitGame()
     {
-        // キャンバスの大きさを設定します
         gc.ChangeCanvasSize(720, 1280);
         gc.SetResolution(720, 1280);
 
@@ -54,21 +42,14 @@ public sealed class Game : GameBase
         resetValue();
     }
 
-    /// <summary>
-    /// 動きなどの更新処理
-    /// </summary>
     public override void UpdateGame()
     {
-        // 起動からの経過時間を取得します
-        // sec = (int)gc.TimeSinceStartup;
         if(gameState == 0){
-            //タイトル画面の処理
             if(gc.GetPointerFrameCount(0) == 1){
                 gameState =1;
             }
         }
         else if(gameState == 1){
-            //ゲーム中の処理
             count++;
             score = count/60;
             enemy_w = 24+count/300;
@@ -95,7 +76,6 @@ public sealed class Game : GameBase
             }
 
             for(int i =0 ; i < ENEMY_NUM ; i ++ ){
-                //箱を動かす処理
                 enemy_y[i] = enemy_y[i] + box_speed[i];
 
                 if(enemy_y[i]> 1280){
@@ -104,70 +84,41 @@ public sealed class Game : GameBase
                     box_speed[i] = gc.Random(3,6);
                 }
 
-                //playerと箱の当り判定
                 if (gc.CheckHitRect (
                     player_x,player_y,170,167,
                     enemy_x[i],enemy_y[i],enemy_w,enemy_h)) {
-                    //当たった時の処理
                     gameState =2;
                     gc.Save("hs",high_score);
                 }
             }
-
-            //playerが画面左右に着いてもgameOverになるように
-            // if(player_x < 0 || player_x > 608){
-            //     gameState =2;
-            //     gc.Save("hs",high_score);
-
-            // }
         }
         else if(gameState == 2){
-            //ゲームオーバー時の処理 
-            //タップしたらスコアを送信(課題２を参考に)
-            // if(gc.GetPointerFrameCount(0) ==1 ){
-            // url = "http://web.sfc.keio.ac.jp/~wadari/sdp/k07_web/score.cgi?score="
-            //     + score + "&name=" + pname;
-            //     gc.GetOnlineTextAsync(url,out str);
-            // }       
             if(gc.GetPointerFrameCount(0) >=120){
                 gameState=0;
                 player_x = 360;
                 player_y = 1100;
                 score = 0;
                 count = 0;
-                // str="";
                 resetValue();
             }
         }
 
     }
 
-    /// <summary>
-    /// 描画の処理
-    /// </summary>
     public override void DrawGame()
     {
-        // 画面を白で塗りつぶします
-        // gc.ClearScreen();
-
-        // gc.DrawOnlineImage("http://web.sfc.keio.ac.jp/~wadari/sdp/k07_web/Player.png",320,240);
-
-        // gc.DrawString(str,0,300);
-
         if(gameState == 0){
-            //タイトル画面の処理
             gc.ClearScreen();
             gc.SetColor(0,0,0);
             gc.SetFontSize(100);
-            gc.DrawString("Shooting Game",40,160);
+            gc.DrawString("Start Screen",50,160);
+            gc.SetFontSize(60);
+            gc.DrawString("Tap screen to start",70,300);
         }
         else if(gameState == 1){
             gc.DrawImage(GcImage.Background,0,0);
-            //ゲーム中の処理
-            // gc.DrawOnlineImage("http://web.sfc.keio.ac.jp/~wadari/sdp/k07_web/Player.png",player_x,player_y);
             gc.DrawImage(GcImage.Flighter,player_x,player_y);
             for(int i =0 ; i < ENEMY_NUM ; i ++ ){
-                // gc.FillRect(enemy_x[i],enemy_y[i],enemy_w,enemy_h); 
                 gc.DrawImage(GcImage.Enemy,enemy_x[i],enemy_y[i]); 
             }
 
@@ -182,10 +133,12 @@ public sealed class Game : GameBase
 
         }
         else if(gameState == 2){
-            //ゲームオーバー時の処理
             gc.SetColor(255,255,255);
             gc.SetFontSize(100);
             gc.DrawString("GAME OVER",140,640);
+            gc.SetFontSize(50);
+            gc.DrawString("Press 2 secs to restart",70,740);
+
 
             // bottom bar
             // gc.SetColor(0,0,0);
@@ -193,18 +146,6 @@ public sealed class Game : GameBase
             // gc.DrawString("SCORE:"+score,0,1240);
             // gc.DrawString("HIGH:"+high_score,360,1240);
         }
-
-        // 青空の画像を描画します
-        // gc.DrawImage(GcImage.BlueSky, 0, 0);
-
-        // // 黒の文字を描画します
-        // gc.SetColor(0, 0, 0);
-        // gc.SetFontSize(48);
-        // gc.SetStringAnchor(GcAnchor.UpperLeft);
-        // gc.DrawString("この文字と青空の画像が", 40, 160);
-        // gc.DrawString("見えていれば成功です", 40, 270);
-        // gc.SetStringAnchor(GcAnchor.UpperRight);
-        // gc.DrawString($"{sec}s", 630, 10);
     }
 
     public void resetValue(){
